a1e11934a27b5473134b84df6af4cb3a0c43b213
Oles Malysh <olesmalish37@gmail.com>
2023-10-24 10:39:27 +0200

    Add a timer

--- app/src/main/java/com/example/guessit/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/guessit/screens/game/GameFragment.kt
@@ -1,10 +1,13 @@
 package com.example.guessit.screens.game
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -18,7 +21,6 @@ import androidx.lifecycle.ViewModelProvider
 /*const val SCORE = "score"*/
 
 class GameFragment : Fragment() {
-
     private lateinit var viewModel: GameViewModel
     private lateinit var binding: GameFragmentBinding
 
@@ -57,12 +59,25 @@ class GameFragment : Fragment() {
             binding.wordText.text = newWord
         }
 
+        viewModel.eventGameFinish.observe(viewLifecycleOwner) { hasFinished ->
+            if (hasFinished) {
+                gameFinished()
+                viewModel.onGameFinishComplete()
+            }
+        }
+
+        viewModel.currentTime.observe(viewLifecycleOwner) {newCurrentTime ->
+            binding.timerText.text = DateUtils.formatElapsedTime(newCurrentTime)
+        }
+
     }
 
 
     fun gameFinished() {
         val action = GameFragmentDirections.actionGameToScore(viewModel.score.value ?: 0)
         findNavController().navigate(action)
+
+      /*  Toast.makeText(this.activity, "Game has finished", Toast.LENGTH_SHORT).show()*/
     }
 
 
--- app/src/main/java/com/example/guessit/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/guessit/screens/game/GameViewModel.kt
@@ -1,11 +1,30 @@
 package com.example.guessit.screens.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+    companion object {
+        // These represent different important times
+        // This is when the game is over
+        const val DONE = 0L
+
+        // This is the number of milliseconds in a second
+        const val ONE_SECOND = 1000L
+
+        // This is the total time of the game
+        const val COUNTDOWN_TIME = 60000L
+    }
+
+    private val timer: CountDownTimer
+
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
 
     // The current word
     private val _word = MutableLiveData<String>()
@@ -14,6 +33,7 @@ class GameViewModel : ViewModel() {
 
     // The current score, can use just inside of this class
     private val _score = MutableLiveData<Int>()
+
     // This variable has override getter and setter so that I can just receive value and can`t to change
     val score: LiveData<Int>
         get() = _score
@@ -22,11 +42,26 @@ class GameViewModel : ViewModel() {
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+        get() = _eventGameFinish
+
     init {
-        Log.i("GameViewModel", "GameViewModel created!")
         resetList()
         nextWord()
         _score.value = 0
+
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = (millisUntilFinished / ONE_SECOND)
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                _eventGameFinish.value = true
+            }
+        }
+        timer.start()
     }
 
 
@@ -59,15 +94,24 @@ class GameViewModel : ViewModel() {
 
     fun nextWord() {
         if (wordList.isEmpty()) {
-            /*gameFinished()*/
+            resetList()
+        }
+        _word.value = wordList.removeAt(0)
+
+
+        /*gameFinished()*//*
+            _eventGameFinish.value = true
+
+
         } else {
             _word.value = wordList.removeAt(0)
-        }
+        }*/
     }
 
     fun onSkip() {
         if (score.value!! > 0) //fix !!
-            _score.value = score.value?.minus(1) //try to use another way.  UPD: tried, not worked, so should use just like here
+            _score.value =
+                score.value?.minus(1) //try to use another way.  UPD: tried, not worked, so should use just like here
         nextWord()
     }
 
@@ -76,4 +120,13 @@ class GameViewModel : ViewModel() {
         nextWord()
     }
 
+    fun onGameFinishComplete() {
+        _eventGameFinish.value = false
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
+
+    }
 }
