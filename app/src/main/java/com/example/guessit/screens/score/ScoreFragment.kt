7507d8dc9c1ad60f7562721619a14dcfe403da88
Oles Malysh <olesmalish37@gmail.com>
2023-10-26 11:35:03 +0200

    Recieve score from ScoreViewModelFactory. It is a way for recieve a params value using MVVM

--- app/src/main/java/com/example/guessit/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/guessit/screens/score/ScoreFragment.kt
@@ -6,15 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.guessit.R
 import com.example.guessit.databinding.ScoreFragmentBinding
-
+import com.example.guessit.screens.game.GameFragmentDirections
 
 
 class ScoreFragment : Fragment() {
     private lateinit var binding: ScoreFragmentBinding
+    private lateinit var viewModel: ScoreViewModel
+    private lateinit var viewModelFactory: ScoreViewModelFactory
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,15 +29,30 @@ class ScoreFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
         super.onViewCreated(view, savedInstanceState)
+        viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(requireArguments()).score)
+        viewModel = ViewModelProvider(this, viewModelFactory) // passing second parameter I say to my ViewModelProvider to use this Fabric for create ScoreViewModel
+            .get(ScoreViewModel::class.java)
 
-        binding.scoreText.text = ScoreFragmentArgs.fromBundle(requireArguments()).score.toString()
-        binding.playAgainButton.setOnClickListener { onPlayAgain() }
+        binding.playAgainButton.setOnClickListener {
+            viewModel.onPlayAgain()
+        }
+
+        viewModel.score.observe(viewLifecycleOwner) {finalScore ->
+            binding.scoreText.text = finalScore.toString()
+        }
+
+        viewModel.eventPlayAgain.observe(viewLifecycleOwner){currentState ->
+            if (currentState){
+                findNavController().navigate(ScoreFragmentDirections.actionRestart())
+                viewModel.gameRestarted()
+            }
+        }
+
     }
 
-    private fun onPlayAgain() {
-        findNavController().navigate(ScoreFragmentDirections.actionRestart())
-    }
 
 
 }
